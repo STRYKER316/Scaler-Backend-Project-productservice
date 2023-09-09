b07ae51146acef2eb2e8c7bc85de833210d06c39
Stryker316 <ansil.mishra316@gmail.com>
2023-09-09 22:28:29 +0530

    services folder added

---
+++ src/main/java/com/example/productservice/services/FakeStoreProductService.java
@@ -0,0 +1,5 @@
+package com.example.productservice.services;
+
+public class FakeStoreProductService implements ProductServiceInterface {
+    
+}
---
+++ src/main/java/com/example/productservice/services/ProductServiceInterface.java
@@ -0,0 +1,5 @@
+package com.example.productservice.services;
+
+public interface ProductServiceInterface {
+    
+}
