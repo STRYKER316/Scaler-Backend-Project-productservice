1baecf4b1693f653f8e0b088bd1aee24716d1f51
Stryker316 <ansil.mishra316@gmail.com>
2023-09-09 21:46:50 +0530

    Controller Folder and ProductController class added to product service

---
+++ src/main/java/com/example/productservice/Controllers/ProductController.java
@@ -0,0 +1,43 @@
+package com.example.productservice.Controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping("/")
+    public void getAllProducts() {
+
+    }
+
+
+    @GetMapping("/{id}")
+    public String getProductById(@PathVariable("id") long id) {
+        return "Here is the product id: " + id;
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteProductById() {
+
+    }
+
+
+    @PostMapping
+    public void createProduct() {
+
+    }
+
+    @PutMapping("/{id}")
+    public void updateProductById() {
+
+    }
+
+}
