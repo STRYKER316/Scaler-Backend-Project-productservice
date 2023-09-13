4658a2da61640bd55caf96724f4bfd4c2b5f7b75
Stryker316 <ansil.mishra316@gmail.com>
2023-09-13 16:40:24 +0530

    Model classes annotated as Entities fo DB, relationships established

--- src/main/java/com/example/productservice/models/BaseModel.java
+++ src/main/java/com/example/productservice/models/BaseModel.java
@@ -1,5 +1,10 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseModel {
+    @Id
     private long id;
 }
--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,14 +1,19 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     private String title;
     private String description;
     private String image;
     private double price;
+//    P : C => M : 1 relation
+    @ManyToOne
     private Category category;
 }
